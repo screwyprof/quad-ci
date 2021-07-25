78313c94ea24dd23e2107f48e0f659779bd299a5
Maksim Shcherbo <max@happygopher.nl>
2021-07-26 00:30:18 +0100

    init

---
+++ test/Spec.hs
@@ -0,0 +1,39 @@
+-- test/Spec.hs
+module Main where
+
+import Core
+import RIO
+import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+
+-- Helper functions
+makeStep :: Text -> Text -> [Text] -> Step
+makeStep name image commands =
+  Step
+    { name = StepName name,
+      image = Image image,
+      commands = NonEmpty.Partial.fromList commands
+    }
+
+makePipeline :: [Step] -> Pipeline
+makePipeline steps =
+  Pipeline {steps = NonEmpty.Partial.fromList steps}
+
+-- Test values
+testPipeline :: Pipeline
+testPipeline =
+  makePipeline
+    [ makeStep "First step" "ubuntu" ["date"],
+      makeStep "Second step" "ubuntu" ["uname -r"]
+    ]
+
+testBuild :: Build
+testBuild =
+  Build
+    { pipeline = testPipeline,
+      state = BuildReady,
+      completedSteps = mempty
+    }
+
+
+main :: IO ()
+main = pure ()
