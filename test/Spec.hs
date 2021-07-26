7ac2a8df3e8ef89b0732489aab116cf9a65e48ba
Maksim Shcherbo <max@happygopher.nl>
2021-07-26 01:46:35 +0100

    Docker: add Runner

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,35 +6,36 @@ import qualified Docker
 import RIO
 import qualified RIO.Map as Map
 import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
-import Test.Hspec
+import qualified Runner
 import qualified System.Process.Typed as Process
+import Test.Hspec
 
 main :: IO ()
 main = hspec do
   docker <- runIO Docker.createService
+  runner <- runIO $ Runner.createService docker
+
   beforeAll cleanupDocker $ describe "Quad CI" do
     it "should run a build (success)" do
-      testRunSuccess docker
+      testRunSuccess runner
 
-testRunSuccess :: Docker.Service -> IO ()
-testRunSuccess docker = do
-  result <- runBuild docker testBuild
+testRunSuccess :: Runner.Service -> IO ()
+testRunSuccess runner = do
+  build <-
+    runner.prepareBuild $
+      makePipeline
+        [ makeStep "First step" "ubuntu" ["date"],
+          makeStep "Second step" "ubuntu" ["uname -r"]
+        ]
+  result <- runner.runBuild build
+
   result.state `shouldBe` BuildFinished BuildSucceeded
-  Map.elems result.completedSteps `shouldBe` [StepSucceeded, StepSucceeded]
+  Map.elems result.completedSteps
+    `shouldBe` [StepSucceeded, StepSucceeded]
 
 cleanupDocker :: IO ()
 cleanupDocker = void do
   Process.readProcessStdout "docker rm -f $(docker ps -aq --filter \"label=quad\")"
-
-runBuild :: Docker.Service -> Build -> IO Build
-runBuild docker build = do
-  newBuild <- Core.progress docker build
-  case newBuild.state of
-    BuildFinished _ ->
-      pure newBuild
-    _ -> do
-      threadDelay (1 * 1000 * 1000)
-      runBuild docker newBuild
 
 -- Helper functions
 makeStep :: Text -> Text -> [Text] -> Step
